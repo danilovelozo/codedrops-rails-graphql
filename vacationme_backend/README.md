a8768da64d47fcf1d2d1cc2d56f6c247901aec66
Danilo Velozo <velozo.dan@gmail.com>
2021-01-18 16:42:44 -0300

    Add Project

---
+++ vacationme_backend/README.md
@@ -0,0 +1,1 @@
+## Rails 6 API Featuring Graphql
---
+++ vacationme_backend/db/seeds.rb
@@ -0,0 +1,11 @@
+User.create(
+  name: 'John Doe',
+  username: 'JohnDoe',
+  password: '123'
+)
+
+User.create(
+  name: 'Ethan Gustafson',
+  username: 'GoodGuyGuf',
+  password: '123'
+)
